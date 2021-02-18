e80a0aa14115ea2471dcad5c6f1b5415ee03c726
Peter Anderson <peterburtonanderson@gmail.com>
2021-02-18 15:09:36 -0700

    Session authentication works - user can login with username or email

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,42 +1,9 @@
 const router = require('express').Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
 
-// GET /api/set-token-cookie
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+router.use('/session', sessionRouter);
 
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        },
-    })
-    setTokenCookie(res, user);
-    return res.json({ user });
-}));
-
-// router.post('/test', (req, res) => {
-//     res.json({ reqBody: req.body });
-// });
-
-// GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
-router.get(
-    '/restore-user',
-    restoreUser,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
-
-// GET /api/require-auth
-const { requireAuth } = require('../../utils/auth.js');
-router.get(
-    '/require-auth',
-    requireAuth,
-    (req, res) => {
-        return res.json(req.user);
-    }
-);
+router.use('/users', usersRouter);
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const apiRouter = require('./api');
 
-router.get('/hello/world', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('HelloWorld!');
-});
-
 router.use('/api', apiRouter);
+
+
+router.get('/hello/world', function (req, res) {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.send('Hello World!');
+});
 
 module.exports = router;
