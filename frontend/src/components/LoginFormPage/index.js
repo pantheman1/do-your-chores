5872a8078d8179996e66aaf3eb479d5bb5147a39
Peter Anderson <peterburtonanderson@gmail.com>
2021-02-20 22:47:57 -0700

    more basic styling of the login form

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -41,33 +41,43 @@ function LoginFormPage({ isLoaded }) {
         <div className="login-body">
             <div className="login-card">
                 <div className="logo-container">
-                    <h2 className="logo-text">Do Your <span className="chores">Chores!</span></h2>
-                    <form onSubmit={handleSubmit}>
-                        <ul>
-                            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-                        </ul>
-                        <label>
-                            Username or Email
-                    <input
-                                type="text"
-                                value={credential}
-                                onChange={(e) => setCredential(e.target.value)}
-                                required
-                            />
-                        </label>
-                        <label>
-                            Password
-        <input
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                required
-                            />
-                        </label>
-                        <button type="submit">Log In</button>
-                        <button type="button" onClick={handleDemoSubmit}>Demo Login</button>
-                        <button type="button" onClick={handleSignup}>Sign up</button>
-                    </form>
+                    <div className="logo-text">Do Your <span className="chores">Chores!</span></div>
+                    <div className="login-form">
+                        <form onSubmit={handleSubmit}>
+                            <ul>
+                                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                            </ul>
+                            <div className="username-container">
+                                <label>Username or Email</label>
+                                <input
+                                    className="username-input"
+                                    type="text"
+                                    value={credential}
+                                    onChange={(e) => setCredential(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className="password-container">
+                                <label>Password</label>
+                                <input
+                                    className="password-input"
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className="login-buttons">
+                                <div className="login-btn">
+                                    <button id="login-btn" type="submit">Log In</button>
+                                </div>
+                                <div className="demo-btn">
+                                    <button id="demo-btn" type="button" onClick={handleDemoSubmit}>Demo Login</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div className="signup-link">Don't have an account? Click here to <NavLink to="/signup">Sign up</NavLink></div>
+                    </div>
                 </div>
             </div>
         </div>
