c5941edfb308f84a742eddab884587572eded068
Peter Anderson <peterburtonanderson@gmail.com>
2021-03-05 15:53:17 -0700

    zoneId shows up --checking diff

---
+++ frontend/src/store/completionStatus.js
@@ -0,0 +1,58 @@
+import { csrfFetch } from './csrf';
+
+const COMPLETED_CHORES = 'completionStatus/COMPLETED_CHORES';
+const INCOMPLETE_CHORES = 'completionStatus/INCOMPLETE_CHORES';
+
+const completedChoresAction = choreList => ({
+    type: COMPLETED_CHORES,
+    choreList
+});
+
+const incompleteChoresAction = (chore) => ({
+    type: INCOMPLETE_CHORES,
+    chore
+})
+
+//Grab all complete chores
+export const completedChores = zoneId => async (dispatch) => {
+    console.log(`/api/chores/${zoneId}/completed`)
+    const res = await csrfFetch(`/api/chores/${zoneId}/completed`);
+
+    if (res.ok) {
+        const chores = await res.json();
+        console.log("LOONEY TOON--->>>", chores)
+        dispatch(completedChoresAction(chores))
+    }
+}
+
+//Grab all incomplete chores
+export const incompleteChores = zoneId => async (dispatch) => {
+    const res = await csrfFetch(`/api/chores/${zoneId}/incomplete`);
+
+    if (res.ok) {
+        const chores = await res.json();
+        dispatch(incompleteChoresAction(chores))
+    }
+}
+
+const CompletedChoresReducer = (state = {}, action) => {
+    console.log("-->>pepep", action.allChores)
+    switch (action.type) {
+        case COMPLETED_CHORES:
+            const allChores = {};
+            action.choreList.forEach(chore => {
+                allChores[chore.id] = chore;
+            });
+            return { ...state, ...allChores }
+        case INCOMPLETE_CHORES:
+            return {
+                ...state,
+                ...action.newChore
+            }
+        default:
+            return state;
+    }
+}
+
+
+export default CompletedChoresReducer;
