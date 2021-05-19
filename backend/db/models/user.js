638595a0ca2f45786e7a7669fe61452867c4ea86
Peter Anderson <peterburtonanderson@gmail.com>
2021-05-19 15:06:36 -0600

    Update db with OwnerSquad model and relationships

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -54,13 +54,21 @@ module.exports = (sequelize, DataTypes) => {
       },
     });
   User.associate = function (models) {
-    const columnMapping = {
+    User.hasMany(models.Chore, { foreignKey: "userId" })
+
+    const columnMappingUserSquad = {
       through: 'UserSquad',
       otherKey: 'squadId',
       foreignKey: 'userId',
     }
-    User.hasMany(models.Chore, { foreignKey: "userId" })
-    User.belongsToMany(models.Squad, columnMapping)
+    User.belongsToMany(models.Squad, columnMappingUserSquad)
+
+    const columnMappingOwnerSquad = {
+      through: 'UserSquad',
+      otherKey: 'squadId',
+      foreignKey: 'userId',
+    }
+    User.belongsToMany(models.Squad, columnMappingOwnerSquad)
   };
   User.prototype.toSafeObject = function () {
     const { id, username, email, squad_id, name } = this;
